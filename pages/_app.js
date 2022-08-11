e95ee9f4ce5fbf1c1b9a44c418d26e15a66c5bd7
Md Dalim <miamddalim669@gmail.com>
2022-08-11 22:07:11 +0600

    edit in app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,26 +1,10 @@
 import "../styles/globals.css";
-import { useState, useEffect } from "react";
-import { RecoilRoot } from "recoil";
-
 function MyApp({ Component, pageProps }) {
-  const [showing, setShowing] = useState(false);
-  useEffect(() => {
-    return () => {
-      setShowing(true);
-    };
-  }, []);
-  if (!showing) {
-    return null;
-  }
-  if (typeof window === "undefined") {
-    return <></>;
-  } else {
-    return (
-      <RecoilRoot>
-        <Component {...pageProps} />
-      </RecoilRoot>
-    );
-  }
+  return (
+    <>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
